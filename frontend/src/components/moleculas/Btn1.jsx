a10bf85b7205993789aab1c2e55107afbfe99af5
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-03-20 23:38:44 -0600

    Se hizo una implementacion inicial de la libreria de styled components

---
+++ frontend/src/components/moleculas/Btn1.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+export const Btn1 = () => {
+    return (
+        <Button>Button</Button>
+    )
+}
+
+const Button = styled.button`
+    background-color: blue;
+    color: white;
+`
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,5 @@
 export { default as App } from './App';
+export * from './components/moleculas/Btn1';
+export * from './index';
 export * from './main';
+export * from './styles/GlobalStyles';
