c8519b3c3b8b19aca1703edeba4ef15175f06738
hcordero-2023253 <hcordero-2023253@kinal.edu.gt>
2025-03-29 22:13:26 -0600

    Se sube el header de hoteleria

--- frontend/src/components/organismos/SiteHeader.jsx
+++ frontend/src/components/organismos/SiteHeader.jsx
@@ -1,14 +1,54 @@
 import styled from "styled-components"
+import { Btn2 } from "../moleculas/Btn.Header"
+import { DiAptana } from "react-icons/di";
+import { CgExtensionAdd } from "react-icons/cg";
+import { BiBell } from "react-icons/bi";
+import { VscAccount } from "react-icons/vsc";
+import { Btn1 } from "../moleculas/Btn1";
+import {logo} from "../../utils/assets/img/FotoDePerfil.jpg"
 
 export const DashboardHeader = () => {
     return (
         <Container>
-            <h1>Header</h1>
+            <Btn2></Btn2>
+            <Btn1></Btn1>
+            <CgExtensionAdd className="Icon"/>
+            <DiAptana className="Config"/>
+            <BiBell  className="Notification"/>
+            <div className="separator" />
+            <VscAccount className="Profile"/>
+            <logo/>
         </Container>
     )
 }
 
 const Container = styled.div`
-    background-color: yellow;
+    background-color: ${({theme})=>theme.colorBackground};
     display: flex;
+    gap: 5px; 
+    padding: 10px;
+
+    .Icon{
+        font-size: 50px;
+        color: ${({theme})=>theme.colorIcon};
+        margin-left: auto
+    }
+    .Config{
+        font-size: 50px;
+        color: ${({theme})=>theme.colorIcon};
+    }
+    .Notification{
+        font-size: 50px;
+        color: ${({theme})=>theme.colorIcon};
+    }
+    .separator {
+        border-left: 2px solid ; 
+        height: 50px; 
+        margin: 0 10px; 
+        color: ${({theme})=>theme.colorIcon};
+    }
+    .Profile{
+        font-size: 50px;
+        color: ${({theme})=>theme.colorIcon};
+    }
 `
--- frontend/src/styles/Theme.jsx
+++ frontend/src/styles/Theme.jsx
@@ -1,9 +1,16 @@
 export const Light ={
     bg: '#d4d1d1',
     color: 'black',
+    colorHover: 'blue',
+    colorBackground: '#e7ebec',
+    colorIcon: 'balck'
+
 }
 
 export const Dark = {
     bg: '#494848',
-    color: 'white',
+    color: 'white', 
+    colorHover: '#ffcc00',
+    colorBackground: '#0f1429 ',
+    colorIcon: 'white'
 }
